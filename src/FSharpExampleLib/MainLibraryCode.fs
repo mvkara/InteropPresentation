9fa766407c9fe427f8ab0f9fc13491caf2ba3b2a
mvkra <mvkra@users.noreply.github.com>
2016-07-18 20:37:36 +1000

    updated slides + some more examples

--- src/FSharpExampleLib/MainLibraryCode.fs
+++ src/FSharpExampleLib/MainLibraryCode.fs
@@ -2,19 +2,24 @@
 // This file contains my example library code written in F#
 
 open System
+open System.Collections.Generic
 open System.Runtime.CompilerServices
 
-// A singly linked-list data structure. F# can be more concise for data structures and core library work.    
+/// A singly linked-list data structure. F# can be more concise for data structures and core library work. 
+/// Note this is pretty similar to the F# built-in list.   
 type public RecursiveList<'t> = 
     | Head of item:'t * tail: RecursiveList<'t>
     | Empty
+    // Some interop methods to allow the type to be useable as is from C#
     member x.Match(headHandler: Func<'t, RecursiveList<'t>, _>, emptyHandler: Func<_>) =
         match x with
         | Head(i, t) -> headHandler.Invoke(i, t)
         | Empty -> emptyHandler.Invoke()
     override x.ToString() = sprintf "%A" x
 
-// Lets follow the F# convention and define some higher order functions in an accompanying module
+/// Lets follow the F# convention and define some higher order functions in an accompanying module.
+/// Most F# names follow standard naming conventions.
+/// There is a bug in these functions (reverse order) but left for simplicity.
 module public RecursiveList =
 
     /// Prepends an item to the list I've defined
@@ -35,11 +40,23 @@ module public RecursiveList =
                 mapRec newList cl
             | Empty -> acc
         mapRec Empty l
+    
+    // Factory method to build a sequence
+    [<CompiledName("OfSeq")>]
+    let public ofSeq s = s |> Seq.fold (fun s t -> s |> cons t) emptyList
 
-/// The C# friendly API. Using extension methods to expose it to LINQ query syntax
+/// The C# friendly API. Using extension methods to expose it to LINQ query syntax.
+/// Make sure to give it friendly C# names which tend to mirror SQL terminology.
 [<Extension>]
 type public CSharpInterop = 
     [<Extension>]
     static member public Select<'t, 'r>(list: RecursiveList<'t>, funcSelector: Func<'t, 'r>) : RecursiveList<'r> =  
         RecursiveList.map funcSelector.Invoke list
+    [<Extension>]
+    static member public Prepend<'t>(list: RecursiveList<'t>, item: 't) : RecursiveList<'t> =  
+        RecursiveList.cons item list
+    [<Extension>]
+    static member public ToRecursiveList<'t>(list: seq<'t>, item: 't) : RecursiveList<'t> =  
+        RecursiveList.ofSeq list
+
 
